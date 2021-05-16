f74829f99a9e6a08e091468b5f5a40aed7beb7c2
Taha Kavas <tahakavas@outlook.com>
2021-05-16 17:39:42 +0300

    16.05.2021

--- methods/Collections/Program.cs
+++ methods/Collections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Collections
 {
@@ -6,14 +7,23 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            string[] names = new string[] { "Engin", "Murat", "Kerem", "Taha" };
-            Console.WriteLine(names[0]);
-            Console.WriteLine(names[1]);
-            Console.WriteLine(names[2]);
-            Console.WriteLine(names[3]);
+            ////string[] names = new string[] { "engin", "murat", "kerem", "taha" };
+            ////console.writeline(names[0]);
+            ////console.writeline(names[1]);
+            ////console.writeline(names[2]);
+            ////console.writeline(names[3]);
 
-            List
-      
+            List<string> names2 = new List<string> { "Engin", "Murat", "Kerem", "Taha" };
+            Console.WriteLine(names2[0]);
+            Console.WriteLine(names2[1]);
+            Console.WriteLine(names2[2]);
+            Console.WriteLine(names2[3]);
+            names2.Add("İlker");
+            Console.WriteLine(names2[4]);
+            Console.WriteLine(names2[0]);
+            
+
+            //Arraylere sonradan ekleme yapılmaz, bu yüzden Listeler kullanırız.
         }
     }
 }
